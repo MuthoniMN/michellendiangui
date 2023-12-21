2c3c4789e78ef9cc3541dc567f5a286a08ab8256
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-12-21 14:17:19 +0300

    adding footer and fontawesome

---
+++ src/app/components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer className="text-center p-5 border-t-2 border-t-stone-700">
+            <p>Website Developed by Michelle Ndiangui</p>
+            <p>Check out my <a href="https://www.github.com/MuthoniMN">GitHub</a></p>
+        </footer>
+    )
+}
